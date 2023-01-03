d7065187be50793f747d48c99aa208df10cfaa14
Kerim Sangrar <kerimsangrar@Kerims-MacBook-Pro.local>
2023-01-03 20:42:45 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef } from 'react';
 
 function App() {
+
+  const [randomInput, setRandomInput] = useState(' ');
+  const [seconds, setSeconds] = useState(0);
+
+  const renders = useRef(0);
+  const timerId = useRef();
+
+  const handleChange = (e) => {
+    setRandomInput(e.target.value);
+    renders.current++;
+  }
+
+  const startTimer = () => {
+    timerId.current = setInterval(() => {
+      renders.current++;
+      setSeconds(prev => prev + 1)
+    }, 1000);
+  }
+
+  const stopTimer =() => {
+    clearInterval(timerId.current);
+    timerId.current=0;
+  }
+
+  const resetTimer = () => {
+    stopTimer();
+    if (seconds) {
+      renders.current++;
+      setSeconds(0);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <input
+        type="text"
+        value={randomInput}
+        placeholder="Random Input"
+        onChange={handleChange}
+      />
+      <p>Renders: {renders.current}</p>
+      <br /><br />
+      <section>
+      <button onClick={startTimer}>Start</button>
+ 
+      <button onClick={stopTimer}>Stop</button>
+
+      <button onClick={resetTimer}>Reset</button>
+      </section>
+      <br /><br />
+      <p>Seconds: {seconds}</p>
+      <br /><br />
+
+
+
+      <p>{randomInput}</p>
+    </main>
   );
 }
 
